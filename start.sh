d7b09214e2dc1b30fdbd11ba5827a37d63a04203
joycemacksuele <jfreitas@student.s19.be>
2023-07-02 11:59:03 +0200

    trying to run everything in a container

--- start.sh
+++ start.sh
@@ -5,10 +5,12 @@
 ############################ Colors ##############################
 bold_black='\033[1;30m'
 bold_red='\033[1;31m'
+green='\033[0;32m'
 bold_green='\033[1;32m'
 bold_yellow='\033[1;33m'
 bold_dark_blue='\033[1;34m'
 bold_purple='\033[1;35m'
+light_blue='\033[0;36m'
 bold_light_blue='\033[1;36m'
 bold_white='\033[1;37m'
 bold_cyan_filled='\033[1;7;96m'
@@ -19,25 +21,25 @@ reset='\033[0m'
 
 
 ################### Install Docker Desktop ###################
-echo -ne "$bold_white Checking if Docker Desktop is installed...\n"
+echo -ne "$bold_white -> Checking if Docker Desktop is installed...\n"
 which docker > /dev/null
 if [[ $? == 0 ]] ; then
-	echo -ne "$bold_green Docker Desktop is already installed!\n"
+	echo -ne "$green Docker Desktop is already installed!\n"
 	echo -e "$reset ------------------------------------\n"
 else
 	echo -ne "$bold_red Docker Desktop is not installed! Please install it and run setup.sh again.\n\n"
-	echo -ne "$bold_dark_blue To install Docker Deskto on a 42 machine: Download Docker Desktop with MSC and select destination to goinfre.\n\n"
+	echo -ne "$light_blue To install Docker Deskto on a 42 machine: Download Docker Desktop with MSC and select destination to goinfre.\n\n"
 	echo -e "$reset ------------------------------------\n"
 fi
 
 
 ################### Downloading npm dependencies ###################
-echo -ne "$bold_white downloading dependencies for the backend:\n"
+echo -ne "$bold_white -> Downloading dependencies for the backend:\n"
 echo -e "$reset ------------------------------------"
 cd ./backend/
 npm install
 
-echo -ne "\n$bold_white downloading dependencies for the frontend:\n"
+echo -ne "\n$bold_white -> Downloading dependencies for the frontend:\n"
 echo -e "$reset ------------------------------------"
 cd ../frontend/
 npm install
@@ -46,6 +48,10 @@ cd ../
 
 
 ################### Building docker compose ###################
-echo -ne "\n$bold_white While developing: docker compose up\n"
+echo -ne "$bold_white -> Building docker compose:\n"
+echo -ne "$reset While developing: docker compose up\n"
 echo -e "$reset ------------------------------------\n"
-docker compose up --build
+docker compose up --build --remove-orphans
+# Found orphan containers ([transcendence_42-react-app-1 transcendence_42-nest-app-1 transcendence_42-database_host-1])
+# for this project. If you removed or renamed this service in your compose file, you can run this command with the
+# --remove-orphans flag to clean it up.
