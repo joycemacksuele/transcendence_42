ccdc409c6d44c5843aeec225ef04bc77eab03283
jaka <jaka72@gmail.com>
2023-07-21 15:56:25 +0200

    Looking at the code, why is index.html not in the public folder

--- backend/src/example.controller.ts
+++ backend/src/example.controller.ts
@@ -6,6 +6,7 @@ export class ExampleController {
   constructor() {
     console.log('[LOG] ExampleController constructor');
   }
+
   @Get('/example')
   getExample(): string {
     return 'This is the response from "example" endpoint.';
