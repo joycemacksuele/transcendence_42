5525c0088ffb8d57f74c191422d3031af5258509
joycemacksuele <jfreitas@student.s19.be>
2023-07-22 17:22:09 +0200

    added pseudo chat

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,26 +1,36 @@
 /* 
     This file is called a 'Controller', for the entity 'user'
-	  It contains logic for handling the incoming HTTP requests of the entity 'user'
+	It contains logic for handling the incoming HTTP requests of the entity 'user'
 
-    Each method in this file needs a corresponding method in the files:  user.service.ts (to interact with the database)
-            user.repository.ts (to handle data operations)
+    Each method in this file needs a corresponding method in the files:
+      - user.service.ts (to interact with the database)
+      - user.repository.ts (to handle data operations)
 
     @Get()  @Post() ... are called 'route decorators'
     For example, the method getAllUsers() is 'decorater with a @Get()'
     When a GET method comes to the route 'users', then the method getAllUsers() is automatically called
     
-    The class is exported, and then this file needs to be imported to the corresponding module (ie: app.module.ts), so that there it enables the routing and request handling.
+    The class is exported, and then this file needs to be imported to the corresponding module
+    (ie: app.module.ts), so that there, it enables the routing and request handling.
 
     Promises are used to handle operations that may take a longer time to complete.
 
     PROMISES
-    When working with asynchronous functions, Promises are used to handle operations that may take a longer time to complete.
+    ------------------------------------
+    When working with asynchronous functions, Promises are used to handle operations that may take a
+    longer time to complete.
 
-    The Promise<MyUser[]> is used to handle the result of an asynchronous function getAllUsers(). This function will fetch a collection of MyUser objects from a database.
+    The Promise<MyUser[]> is used to handle the result of an asynchronous function getAllUsers().
+    This function will fetch a collection of MyUser objects from a database.
 
-    A Promise<MyUser[]> allows the function to start retrieving the users asynchronously. The Promise object represents the eventual completion of retreiving. The Promise will become an array of MyUser objects.
+    A Promise<MyUser[]> allows the function to start retrieving the users asynchronously.
+    The Promise object represents the eventual completion of retreiving.
+    The Promise will become an array of MyUser objects.
 
-    The code that calls getAllUsers() can then handle the Promise using methods like .then() or await to access the resolved value (the array of MyUser objects) when it becomes available. This way, the code can continue executing other tasks while waiting for the asynchronous operation to complete, improving the overall efficiency and responsiveness of the application.
+    The code that calls getAllUsers() can then handle the Promise using methods like .then() or await to
+    access the resolved value (the array of MyUser objects) when it becomes available.
+    This way, the code can continue executing other tasks while waiting for the asynchronous operation
+    to complete, improving the overall efficiency and responsiveness of the application.
 */
 
 import {Controller, Post, Get, Body, Logger, Delete, Param, HttpCode} from '@nestjs/common';
@@ -28,26 +38,25 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './create-user.dto';
 import { MyUser } from './user.entity';
 
-
 @Controller('users')    // the name must correspond to the path in frontend function get in userList
 export class UserController {
 
   private readonly logger = new Logger(UserController.name);
 
   constructor(private readonly userService: UserService) {
-    console.log('[LOG] UserController constructor');
+    console.log('[BACKEND LOG] UserController constructor');
   }
 
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto) {
-    console.log('[LOG] createUser');
-    this.logger.log('[LOG] Received user data:', JSON.stringify(createUserDto));
+    console.log('[BACKEND LOG] createUser');
+    this.logger.log('[BACKEND LOG] Received user data:', JSON.stringify(createUserDto));
     return this.userService.createUser(createUserDto);
   }
   
   @Get()
   async getAllUsers(): Promise<MyUser[]> {
-    console.log('[LOG] getAllUsers');
+    console.log('[BACKEND LOG] getAllUsers');
     return (this.userService.getAllUsers());
   }
 
@@ -57,7 +66,7 @@ export class UserController {
   // It needs the @Param decorator to be able to pass the arg to the function getUserById( id )'
   @Get(':id')
   async getUserById(@Param('id') id: number): Promise<MyUser> {
-    console.log('[LOG] getUserById');
+    console.log('[BACKEND LOG] getUserById');
     return this.userService.getUserById(id);
   }
 
@@ -66,9 +75,9 @@ export class UserController {
   async deleteAllUsers(): Promise<void> {
     try {
       await this.userService.deleteAllUsers();
-      console.log('[LOG] from nest user.controller: All users deleted.');
+      console.log('[BACKEND LOG] from nest user.controller: All users deleted.');
     } catch (error) {
-      console.error('[LOG] from nest user.controller: Error deleting all users.', error);
+      console.error('[BACKEND LOG] from nest user.controller: Error deleting all users.', error);
     }
   }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import App_vite_orig  from './components/example_vite_orig.tsx'
-import SomeText       from './components/ex00_someText.tsx'
-import SomeButtons    from './components/buttons/ex01_someButtons.tsx'
-import ExampleInputField	from './components/forms/RegisterForm.tsx'
-import ExampleFormUserName	from './components/forms/exampleInputUserName.tsx'
-import ExampleDisplayUsers	from './components/forms/exampleDisplayUsers.tsx'
+import App_vite_orig from './components/example_vite_orig.tsx'
+import SomeText from './components/ex00_someText.tsx'
+import SomeButtons from './components/buttons/ex01_someButtons.tsx'
+import ExampleFormUserName from './components/forms/exampleInputUserName.tsx'
+import ExampleDisplayUsers from './components/forms/exampleDisplayUsers.tsx'
+import ChatInputField from './components/forms/chatForm.tsx'
+import './utils/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
@@ -14,9 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 		<App_vite_orig />
 		<SomeText />
 		<SomeButtons />
-		<ExampleInputField />
 		<ExampleFormUserName />
 		<ExampleDisplayUsers />
+		<ChatInputField />
 
 	</React.StrictMode>,
 )
