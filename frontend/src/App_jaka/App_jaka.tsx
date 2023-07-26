022066f975e386c9e233537f84ab3347aa35d7df
jaka <jaka72@gmail.com>
2023-07-26 23:04:04 +0200

    Click on login button jumps to main_page, skips the Aouthorisation link

--- frontend/src/App_jaka/App_jaka.tsx
+++ frontend/src/App_jaka/App_jaka.tsx
@@ -1,44 +1,71 @@
 // import React from "react";
 import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+
 
 import "./css/App_jaka.css";
+
 import LoginPage from "./Components/login_page.tsx";
-import Header from "./Header/Header.tsx";
-import Sidebar from "./Sidebar/Sidebar.tsx";
-import Center from "./Center/Center.tsx";
+import MainPage from "./Components/main_page.tsx";
+// import Header from "./Header/Header.tsx";
+// import Sidebar from "./Sidebar/Sidebar.tsx";
+// import Center from "./Center/Center.tsx";
 
 
-const MainPage = () => {
-  return (
-	<div>
-	  <Header />
-	  <div className="main-grid-container">
-		<Sidebar />
-		<Center />
-	  </div>
-	</div>
-  );
-};
+// const MainPage = () => {
+//   return (
+// 	<div>
+// 	  <Header />
+// 	  <div className="main-grid-container">
+// 		<Sidebar />
+// 		<Center />
+// 	  </div>
+// 	</div>
+//   );
+// };
 
-const App_jaka = () => {
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-	useEffect(() => {
-	  // Check if you have the access token or authorization code in your state or local storage
-	  // For example, you can check localStorage or Redux store
-	  const hasAccessToken = localStorage.getItem('access_token');
-	  if (hasAccessToken) {
-		setIsLoggedIn(true);
-	  }
-	}, []);
 
+const App_jaka: React.FC = () => {
 	return (
-	  <React.StrictMode>
-		{/* {isLoggedIn ? <MainPage /> : <LoginPage />} */}
-		<MainPage />
-	  </React.StrictMode>
-);
-
-
+		<div>
+			<Routes>
+				<Route path="/" element={<LoginPage />} />
+				<Route path="/main_page" element={<MainPage />} />
+			</Routes>
+		</div>
+	);
 };
+
+
+
+
+// OLD (CHANGED TO A SWITCH AND ROUTER)
+// const App_jaka: React.FC = () => {
+// 	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	
+// 	useEffect(() => {
+// 		// Check if you have the access token or authorization code in your state or local storage
+// 		// For example, you can check localStorage or Redux store
+	 
+// 		setIsLoggedIn(false); // jaka: temporary so
+	  
+// 	  const hasAccessToken = localStorage.getItem('access_token');
+// 	  if (hasAccessToken) {
+// 		setIsLoggedIn(true);
+// 	  }
+// 	}, []);
+
+// 	return (
+// 	  <React.StrictMode>
+
+// 		{isLoggedIn ? <MainPage /> : <LoginPage />}
+// 		{/* <MainPage /> */}
+// 	  </React.StrictMode>
+// 	);
+// };
+
+
+
 export default App_jaka;
