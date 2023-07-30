4634f8c41c20fe57ce19f558b115165d6c1a6fb3
jaka <jaka72@gmail.com>
2023-07-30 14:11:43 +0200

    Added input field to change username in the profile page

--- frontend/src/App_jaka/Components/Center/Center.tsx
+++ frontend/src/App_jaka/Components/Center/Center.tsx
@@ -4,25 +4,24 @@ import '../../css/Center.css'
 
 import UserProfilePage from '../userProfilePage';
 import PlayGamePage from '../playGamePage';
-// import StatsPage from '../statsPage';
+import StatsPage from '../stats_page';
 import LogoutPage from '../logoutPage';
 
 
-type HeaderProps = {
-  activeContent: string | null;
+type PropsCenter = {
+  activeContent: string;
 };
 
 
-const Center: React.FC<HeaderProps> = ({ activeContent }) => {
-
+const Center: React.FC<PropsCenter> = ({ activeContent }) => {
   return (
     <div className='main-field'>
       <div id='div-center'>
-        <h3>Center</h3>
+        {/* <h3>Center</h3> */}
 
         {activeContent === 'User Profile Page' && <UserProfilePage /> }
         {activeContent === 'Play Game Page' && <PlayGamePage /> }
-        {/* {activeContent === 'Stats_Page' && <StatsPage /> } */}
+        {activeContent === 'Statistics Page' && <StatsPage /> }
         {activeContent === 'Logout Page' && <LogoutPage /> }
 
 
