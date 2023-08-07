518e29a3413ada21430ddb9232c8b28e377ab3f3
jaka <jaka72@gmail.com>
2023-08-07 18:21:51 +0200

    Added function to create 5 dummy users in the database, and also button to delete all users

--- backend/src/tests/exampleButtons/test.controller.ts
+++ backend/src/tests/exampleButtons/test.controller.ts
@@ -1,6 +1,6 @@
 import {Controller, Post, Get, Body, Logger, Delete, Param, HttpCode} from '@nestjs/common';
-import {AppService} from "../app/app.service";
-import { CreateUserDto } from '../user/create-user.dto';
+import {AppService} from "../../app/app.service";
+import { CreateUserDto } from '../../user/create-user.dto';
 
 @Controller()
 export class TestButton {
