8b1fbb95931682efc7bf7fb394df53999b20ee42
jaka <jaka72@gmail.com>
2023-08-09 16:37:41 +0200

    Added button create dummy users and added profile icon to top line

--- backend/src/app/app.service.ts
+++ backend/src/app/app.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor() {
+    constructor(private configService: ConfigService) {
     console.log('[BACKEND LOG] AppService constructor');
   }
 
   getHello(): string {
     console.log('[BACKEND LOG] getHello AppService');
     return '[BACKEND] Hello World!';
+  }
+
+
+  // Jaka: to get values from the .env file
+  getIntraToken(): string {
+    return this.configService.get<string>('TOKEN');
   }
 }
