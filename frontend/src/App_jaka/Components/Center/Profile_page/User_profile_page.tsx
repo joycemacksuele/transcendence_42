6244d1a9132cd0b47b602128d911e0fe1a4ccea3
jaka <jaka72@gmail.com>
2023-08-09 16:37:41 +0200

    Added button create dummy users and added profile icon to top line

--- frontend/src/App_jaka/Components/Center/Profile_page/User_profile_page.tsx
+++ frontend/src/App_jaka/Components/Center/Profile_page/User_profile_page.tsx
@@ -1,18 +1,17 @@
 import InputUserName from "../../Other/InputUserName";
 import UploadAvatar from "../../Other/UploadAvatar";
-import UsersList	from '../../../../components/forms/exampleDisplayUsers'
+import UsersList from "./exampleDisplayUsers";
 
 const UserPage = () => {
   return (
     <div id="user-page">
       <p>USER PAGE</p>
       {/* <p>Change your username: </p> */}
-      < InputUserName />
-      < UploadAvatar />
-      < UsersList />
-
+      <InputUserName />
+      <UploadAvatar />
+      <UsersList />
     </div>
   );
 };
 
-export default UserPage; 
+export default UserPage;
