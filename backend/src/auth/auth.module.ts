a0b9a1d11e34a0cd7aa6258651b04ce9ae0d4e03
Janeway42 <corina.a.popa@gmail.com>
2023-08-21 13:43:06 +0200

    guards

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthGuard } from './guards/auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [JwtModule],
+  imports: [JwtModule.register({
+    global: true
+  })],
+
   controllers: [AuthController],
-  providers: [AuthService]
+  
+  providers: [AuthService,
+  {
+    provide: APP_GUARD,
+    useClass: AuthGuard, 
+  }],
 })
 export class AuthModule {}
