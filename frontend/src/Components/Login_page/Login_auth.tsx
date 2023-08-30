beba412b1d1dd8a2faf601b0cf86af7fa9da3bd1
jaka <jaka72@gmail.com>
2023-08-30 12:36:14 +0200

    Moved the redirect to intra-login page from backend to frontend. To change: Intra needs to first return to frontend, only then use axios to go to backend, then get back the response from backend, then frontend redirects to the main_page

--- frontend/src/Components/Login_page/Login_auth.tsx
+++ frontend/src/Components/Login_page/Login_auth.tsx
@@ -1,8 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-// Jaka, for testing
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 axios.defaults.withCredentials = true; // jaka, is it needed here?
 
@@ -10,52 +7,64 @@ const LoginPage: React.FC = () => {
 	const [response, setResponse] = useState<string>('');
 	// const [response00, setResponse00] = useState<string>('');
 
-	console.log('LoginPage ...');
-	const handleClickAuth = async () => {
-		await sleep(2000);	// jaka, remove
-		try {
-			console.log('Try going to backend ...');
-			alert ('Try going to backend ...')
-			const response = await axios.get('http://localhost:3001/auth/login'); // This goes to nest, auth controller....
-			console.log(response);
-			// const userData = response.data;
-			// console.log(userData);
-			// JAKA, HERE SAVE USER DATA TO LOCAL STORAGE
-			// ...
-			// THIS DOES NOT COME TO BROWSER CONSOLE ...LoginPage.
-			console.log('Jaka, Should redirect to main page ...');
+	const handleClickAuth = () => {
 
-			window.location.href = 'http://localhost:3000/main_page';
-		} catch(error) {
-			console.error('An error occured', error);
-			setResponse('An error occured');
-		}
+		console.log('Redirect from frontend directly to intra ...');
 
-		// axios.get('http://localhost:3001/auth/login')
-		// 	.then((response) => {
-		// 		console.log(response);
-		// 		const userData = response.data.userData;
-		// 		console.log(userData);
-		// 		//setUserData(userData);
-		// 		//setRedirectToMainPage(true);
-		// 	})
-		// 	.catch((error) => {
-		// 		console.error('An error occurred', error);
-		// 		setResponse('An error occurred');
-		// 	});
-
-
-
+		window.location.href = 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-d56d700e9560937acc2eb4461b7fc08f12e39e060503cc22ea59b952aa77d806&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fauth%2Ftoken&response_type=code';
+		
+		console.log('Jaka, A) ...');	
 	};
 
+	useEffect(() => {
+		const urlParams = new URLSearchParams(window.location.search);
+		const code = urlParams.get('code');
+	
+		if (code) {
+
+			axios
+				.get(`http://localhost:3001/auth/token?code=${code}`) // This goes to nest, auth controller....
+				//   .then((response) => setResponse(response.data))
+				// .then(response => response.data())
+				.then(response => {
+					console.log(response);
+					const userData = response.data;
+					console.log(userData);
+					// JAKA, HERE SAVE USER DATA TO LOCAL STORAGE
+					// ...
+					// THIS DOES NOT COME TO BROWSER CONSOLE ...LoginPage.
+					console.log('Jaka, Should redirect to main page ...');
+					
+					window.location.href = 'http://localhost:3000/main_page';
+				})
+				
+				.catch(error => {
+					console.error('An error occured', error);
+					setResponse('An error occured');
+				});
+			}
+			
+			console.log('Jaka, B) ...');
+		}, []);
+		
+		console.log('Jaka, C) ...');
+
+
+
+	// const handleClickTest00 = () => {
+	// 	axios
+	// 	  .get('http://localhost:3001/example') // This goes to nest, example controller....
+	// 	  .then((response00) => setResponse00(response00.data))
+	// 	  .catch((err) => console.error(err));
+	//   };
 
   return (
 	<>
     <div>
-      <h1>This is Login Page for Aouth</h1>
-      {/* <button onClick={handleClickAuth}>Login with OAuth</button> { response } */}
+      <h1>This is Login Page for Aouthx</h1>
+      <button onClick={handleClickAuth}>Login with OAuth</button> { response }
       {/* <button onClick={handleClickAuth}>Login with OAuth</button> */}
-	  <a href='http://localhost:3001/auth/login'>Click to Login</a>
+	  {/* <a href='http://localhost:3001/auth/login'>Click to Login</a> */}
     </div>
 	{/* <div>
 		<button onClick={handleClickTest00}>Test Button 00</button> { response00 }
---
+++ frontend/src/Components/Login_page/Login_authORIG.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+// Jaka, for testing
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+axios.defaults.withCredentials = true; // jaka, is it needed here?
+
+const LoginPage: React.FC = () => {
+	const [response, setResponse] = useState<string>('');
+	// const [response00, setResponse00] = useState<string>('');
+
+	console.log('LoginPage ...');
+	const handleClickAuth = async () => {
+		//await sleep(2000);	// jaka, remove
+		try {
+			console.log('Try going to backend ...');
+			//alert ('Try going to backend ...')
+			const response = await axios.get('http://localhost:3001/auth/login'); // This goes to nest, auth controller....
+			console.log(response);
+			// const userData = response.data;
+			// console.log(userData);
+			// JAKA, HERE SAVE USER DATA TO LOCAL STORAGE
+			// ...
+			// THIS DOES NOT COME TO BROWSER CONSOLE ...LoginPage.
+			console.log('Jaka, Should redirect to main page ...');
+
+			window.location.href = 'http://localhost:3000/main_page';
+		} catch(error) {
+			console.error('An error occured', error);
+			setResponse('An error occured');
+		}
+
+		// axios.get('http://localhost:3001/auth/login')
+		// 	.then((response) => {
+		// 		console.log(response);
+		// 		const userData = response.data.userData;
+		// 		console.log(userData);
+		// 		//setUserData(userData);
+		// 		//setRedirectToMainPage(true);
+		// 	})
+		// 	.catch((error) => {
+		// 		console.error('An error occurred', error);
+		// 		setResponse('An error occurred');
+		// 	});
+
+
+
+	};
+
+
+  return (
+	<>
+    <div>
+      <h1>This is Login Page for Aouth</h1>
+      <button onClick={handleClickAuth}>Login with OAuth</button> { response }
+      {/* <button onClick={handleClickAuth}>Login with OAuth</button> */}
+	  {/* <a href='http://localhost:3001/auth/login'>Click to Login</a> */}
+    </div>
+	{/* <div>
+		<button onClick={handleClickTest00}>Test Button 00</button> { response00 }
+  		</div>*/}
+	</>
+  );
+};
+
+export default LoginPage;
