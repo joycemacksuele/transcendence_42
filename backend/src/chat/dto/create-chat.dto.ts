287d49f8ad5ad1868a15511bbc3efac535d4c23f
joycemacksuele <jfreitas@student.s19.be>
2023-09-24 13:06:24 +0200

    JOyce - trying to merge to main

---
+++ backend/src/chat/dto/create-chat.dto.ts
@@ -0,0 +1,34 @@
+import { IsNotEmpty, IsOptional, IsString, MinLength, MaxLength } from 'class-validator';
+
+export enum ChatType {
+    PUBLIC,
+    PRIVATE,
+    PROTECTED,//by a password
+}
+
+export class CreateChatDto {
+    @IsNotEmpty({ message: 'Required' })
+    @IsString()
+    @MinLength(5)
+    @MaxLength(10)
+    roomName: string;
+
+    @IsNotEmpty({ message: 'Required' })
+    roomId: number;
+
+    @IsNotEmpty({ message: 'Required' })
+    creatorId: number;
+
+    @IsNotEmpty({ message: 'Required' })
+    adminId: number;
+
+    @IsNotEmpty({ message: 'Required' })
+    roomType: ChatType;
+
+    // Only has a password if its a type PROTECTED
+    @IsOptional()
+    @IsString()
+    @MinLength(5)
+    @MaxLength(10)
+    roomPassword: string;
+}
