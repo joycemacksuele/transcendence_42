9224310c93236f743ce3e18e55755cc8f2ea174b
jaka72 <54038119+jaka72@users.noreply.github.com>
2023-10-26 17:10:58 +0200

    Delete frontend/src/Components/Login_page/login_2fa.tsx

--- frontend/src/Components/Login_page/login_2fa.tsx
+++
@@ -1,5 +0,0 @@
-// this page has to contain a form with 
-
-1 box to input the code received by email 
-1 button - verify code -> Post request to http://localhost:3001/2fa/verify_code 
-1 button - resend code ->Post request to http://localhost:3001/2fa/resend_code 
