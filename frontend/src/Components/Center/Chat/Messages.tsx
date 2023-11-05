53d77a149c6a49e195487acd33a16f1c337751fb
Joyce Macksuele <joycemacksuele@macbook-air.local>
2023-11-05 17:29:44 +0100

    trying to make onchange for the chattype

--- frontend/src/Components/Center/Chat/Messages.tsx
+++ frontend/src/Components/Center/Chat/Messages.tsx
@@ -1,8 +1,4 @@
-// TODO: EACH USER SHOWN ON THE CHAT SCREEN HAS TO BE CLICKABLE AND BRING THE USER TO THIS USER'S PUBLIC PROFILE PAGE
-
-import React, { useState, useEffect } from 'react';
-import { Socket, io } from "socket.io-client";
-import $ from "jquery";
+import React, { useState } from 'react';
 
 // Stylesheets: Because React-Bootstrap doesn't depend on a very precise version of Bootstrap, we don't
 // ship with any included CSS. However, some stylesheet is required to use these components:
@@ -15,16 +11,10 @@ import '../../../css/Chat.css'
 // import avatarImage from '../../../images/avatar_default.png'
 
 // Importing bootstrap and other modules
-import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Stack from 'react-bootstrap/Stack';
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Nav';
 
 const Messages = () => {
 
@@ -33,7 +23,7 @@ const Messages = () => {
     const [message, setMessage] = useState('');
     const [messageBoxPlaceHolder, setMessageBoxPlaceHolder] = useState('Write a message...');
 
-    const sendMessage = async (event: React.FormEvent) => {
+    const sendMessage = async (event: React.FormEvent)=> {
         event.preventDefault();
         if (message.trim() == '') {
             setMessageBoxPlaceHolder('Please write a message.');
@@ -43,7 +33,7 @@ const Messages = () => {
             try {
                 console.log('BEFORE SENDING TO BACKEND');// TODO I never see this log too I THINK FRONTEND IS NOT LOGGING
 
-                const response = await axios.post('http://localhost:3001/chat', { message });
+                // const response = await axios.post('http://localhost:3001/chat', { message });
                 // make this via socket.emit("SendMessage");
                 // how to send data? send the message + userId to send the message to (or roomId?)
 
@@ -51,7 +41,7 @@ const Messages = () => {
                 setMessageBoxPlaceHolder('Write a message...');
 
                 // console.log(response.data); // Handle the response as needed
-                console.log('Response from the backend in JSON: ', JSON.stringify(response));// TODO I never see this log
+                // console.log('Response from the backend in JSON: ', JSON.stringify(response));// TODO I never see this log
             } catch (error) {
                 console.error('[FRONTEND ERROR] ', error);
             }
