9244d7826f737d287cc0e448d89e1bae5325179c
jaka <jaka72@gmail.com>
2023-11-15 14:48:01 +0100

    Renamed and out-commented unused file ManageUserNames - has to be removed

--- frontend/src/Components/Login_page/ManageUserNamesOLD_NOT_USED.tsx
+++ frontend/src/Components/Login_page/ManageUserNamesOLD_NOT_USED.tsx
@@ -1,29 +1,29 @@
 import axios from 'axios';
 
 
-// The API call to the backend endpoint:
-export const storeLoginName = async (loginName: string) => {
-	try {
-	//   await axios.post('http://localhost:3001/store_curr_user_to_databs', { loginName });
-	  await axios.post('http://localhost:3001/userName/storeLoginName', { loginName });
-	  console.log('Current user stored to database successfully.');
-	} catch (error: any) {
-	  console.error('Error storing current user to database:', error.message);
-	  throw error;
-	}
-	return {};
-  };
+// // The API call to the backend endpoint:
+// export const storeLoginName = async (loginName: string) => {
+// 	try {
+// 	//   await axios.post('http://localhost:3001/store_curr_user_to_databs', { loginName });
+// 	  await axios.post('http://localhost:3001/userName/storeLoginName', { loginName });
+// 	  console.log('Current user stored to database successfully.');
+// 	} catch (error: any) {
+// 	  console.error('Error storing current user to database:', error.message);
+// 	  throw error;
+// 	}
+// 	return {};
+//   };
 
 
-  export const changeProfileName = async (profileName: string) => {
-	try {
-		await axios.post('http://localhost:3001/changeProfileName', profileName);
-	} catch (error: any) {
-		console.error('Error changing the profilename.', error.message);
-		throw error;
-	}
-	return {};
-  };
+//   export const changeProfileName = async (profileName: string) => {
+// 	try {
+// 		await axios.post('http://localhost:3001/changeProfileName', profileName);
+// 	} catch (error: any) {
+// 		console.error('Error changing the profilename.', error.message);
+// 		throw error;
+// 	}
+// 	return {};
+//   };
 
 
 
