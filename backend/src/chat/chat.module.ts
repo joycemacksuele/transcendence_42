4ddf4f6eaef7ab5ace0fa79a38406f2c9d5c6deb
Panoramix de Druïde <panoramix@xn--dru-1ma.de>
2023-11-29 23:00:15 +0100

    Some more work on the database relations

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -8,10 +8,11 @@ import {ChatMessageEntity} from "./entities/chat-message.entity";
 import { UserService } from "src/user/user.service";
 import { UserModule } from "src/user/user.module";
 import { UserEntity } from "src/user/user.entity";
+import { ChatRepository } from "./chat.repository";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([NewChatEntity, ChatMessageEntity, UserEntity]), UserModule],
-  providers: [ChatService, ChatGateway, UserService],
+  imports: [TypeOrmModule.forFeature([NewChatEntity, ChatMessageEntity, UserEntity, ChatRepository]), UserModule],
+  providers: [ChatService, ChatGateway, UserService, ChatRepository],
   controllers: [ChatController],
   // Providers can include services or repositories (and as we won't have a controller for the chat,
   // we can add the chat gateway (for websockets) here too
