4ee6d2db586fe804e49666da1a1e966acf7c9aeb
hman <hman@student.codam.nl>
2023-12-02 15:24:15 +0100

    test

---
+++ backend/src/ponggame/ponggame.gateway.ts
@@ -0,0 +1,160 @@
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  ConnectedSocket,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketServer,
+} from "@nestjs/websockets";
+import { Server, Socket } from "socket.io";
+import { PonggameService } from "./ponggame.service";
+import { GameState } from "./dto/game-state.dto";
+
+import { JwtService } from "@nestjs/jwt";
+
+@WebSocketGateway({
+  cors: {
+    //    origin: '*', //!DEV allowing any origin
+    origin: "http://localhost:3000", // allow the origin http://localhost:5173
+    credentials: true,
+  },
+})
+export class PonggameGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  private server: Server;
+
+  //variables for sending the gamestate to the clients
+  private _gameInterval: NodeJS.Timeout;
+  private _processingGamestates: boolean = false;
+  private _socketIdUserId: Map<string, string> = new Map(); //maps socketId to userId
+
+  private _userIdSocketId: Map<string, string> = new Map(); //maps the userId to a socketId
+  //private _testcounter = 0;
+
+  constructor(
+    private readonly ponggameService: PonggameService,
+    private readonly jwtService: JwtService
+  ) {
+    this._gameInterval = setInterval(() => {
+      if (this._processingGamestates == false) {
+        this._processingGamestates = true;
+        ponggameService.updateCurrentMatches();
+        const currentGames = ponggameService.getCurrentMatches();
+        currentGames.forEach((gamestate: GameState) => {
+          this.server.to(gamestate.roomName).emit("stateUpdate", gamestate);
+        });
+        ponggameService.cleanUpMatches();
+        this._processingGamestates = false;
+        //TEST CODE
+        // this._testcounter += 13;
+        // if (this._testcounter > 1000) {
+        //   console.log('currentGames count ' + currentGames.size);
+        //   ponggameService.test_display_userMatch();
+        //   this._testcounter = 0;
+        // }
+      }
+    }, 13);
+  }
+
+  async handleConnection(client: Socket) {
+    console.log(`pong game client id ${client.id} connected`);
+
+    // const token = client.handshake.headers.cookie.split("=")[1];
+    // if (!token) console.log(">>>>>>>>>>>>>>>.No Token Found<<<<<<<<<<<<<");
+    const token = client.handshake.headers.cookie?.split("=")[1];
+    if (!token) console.log(">>>>>>>>>>>>>>>>>No Token <<<<<<<<<<<<<<<<");
+    else console.log(">>>>>>>>>>>>>Token found <<<<<<<<<<<<<<<<");
+    console.log(token);
+    const payload = await this.jwtService.verifyAsync(token, {
+      secret: process.env.JWT_SECRET,
+    });
+    console.log(payload);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`client id ${client.id} disconnected`);
+    this.ponggameService.playerDisconnected(
+      this._socketIdUserId.get(client.id)
+    );
+    this._userIdSocketId.delete(this._socketIdUserId.get(client.id));
+    this._socketIdUserId.delete(client.id);
+  }
+
+  @SubscribeMessage("identify")
+  identify(@MessageBody() identity: string, @ConnectedSocket() client: Socket) {
+    console.log(
+      `client has been identified as ${identity} with the socket id of ${client.id}`
+    );
+    //register the socketid to the user id
+    this._socketIdUserId.set(client.id, identity);
+    //register the userid to the socketid
+    this._userIdSocketId.set(identity, client.id);
+    //check if the user is already part of a match
+    const matchId = this.ponggameService.getMatchId(identity);
+    if (matchId == "") {
+      //if not get the selection screen
+      client.emit("stateUpdate", this.ponggameService.getInitMatch("Default"));
+    } else {
+      //if part of the game then join the match
+      client.join(matchId);
+    }
+  }
+
+  @SubscribeMessage("joinDefaultGame")
+  joinDefaultGame(
+    @MessageBody() identity: string,
+    @ConnectedSocket() client: Socket
+  ) {
+    console.log(`client requested to join game as ${identity}`);
+    const userId = this._socketIdUserId.get(client.id);
+    console.log("joining default game" + userId);
+    const matchId = this.ponggameService.joinGame(userId, "Default");
+    client.join(matchId);
+  }
+
+  @SubscribeMessage("joinCustomGame")
+  joinCustomGame(
+    @MessageBody() identity: string,
+    @ConnectedSocket() client: Socket
+  ) {
+    console.log(`cleint request to join a custom game as ${identity}`);
+    const userId = this._socketIdUserId.get(client.id);
+    console.log("joining custom game " + userId);
+    const matchId = this.ponggameService.joinGame(userId, "Custom");
+    client.join(matchId);
+  }
+
+  @SubscribeMessage("playerinput")
+  procesInput(@MessageBody() input: number, @ConnectedSocket() client: Socket) {
+    const userId = this._socketIdUserId.get(client.id);
+    const matchId = this.ponggameService.getMatchId(userId);
+    console.log("update" + matchId);
+    if (matchId != "") {
+      this.ponggameService.updateUserInput(matchId, userId, input);
+    }
+  }
+
+  @SubscribeMessage("createPrivateGame")
+  createPrivateGame(
+    @MessageBody() data: { player1: string; player2: string },
+    @ConnectedSocket() client: Socket
+  ) {
+    console.log(`player1 : ${data.player1} player2:${data.player2}`);
+  }
+  //test function
+  print_out_socketId() {
+    console.log("\ncurrent log");
+    this._socketIdUserId.forEach((value, key) => {
+      console.log(`Socket id ${key} userID ${value}`);
+    });
+    console.log("\n");
+
+    this._userIdSocketId.forEach((value, key) => {
+      console.log(`user id ${key} client id ${value}`);
+    });
+    console.log("\n");
+  }
+}
--- frontend/src/Components/Center/Game/Game.tsx
+++ frontend/src/Components/Center/Game/Game.tsx
@@ -1,8 +1,79 @@
+import { useEffect, useRef, useState } from "react";
+import { Socket, io } from "socket.io-client";
+import { GameState } from "./Gamestate";
+import { drawScene } from "./CanvasDraw";
+import GameSelection from "./GameSelection";
 
-const Game = () => {
-	return (
-		<p>PLAY GAME PAGE</p>
-	);
-};
+function Game({ player }: { player: string }) {
+  const apiAddress = "http://localhost:3001";
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const width = window.innerWidth;
+  const height = window.innerHeight - 70;
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [gameState, setGameState] = useState<GameState | undefined>(undefined);
+
+  player = "Testuser";
+  useEffect(() => {
+    const newSocket = io(apiAddress, { transports: ["websocket"] });
+    setSocket(newSocket);
+
+    console.log("socket created");
+    //disconnect socket to clean up
+    return () => {
+      console.log(`socket disconnecting`);
+      socket?.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    socket?.on("connect", () => {
+      socket.emit("identify", player);
+      console.log(`connected to server pong ${socket.id} as player ${player}`);
+    });
+    socket?.on("stateUpdate", (newdata: GameState) => setGameState(newdata));
+
+    //add keylistener
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+    //clean up
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+      socket?.removeAllListeners();
+      socket?.disconnect();
+    };
+  }, [socket]);
+
+  useEffect(() => {
+    if (gameState != undefined) drawScene(canvasRef, gameState);
+  }, [gameState]);
+
+  function handleKeyDown(event: KeyboardEvent) {
+    if (event.repeat) return;
+    //console.log("keypressed " + event.code);
+    if (event.code == "KeyA") {
+      socket?.emit("playerinput", 1);
+    } else if (event.code == "KeyQ") {
+      socket?.emit("playerinput", -1);
+    }
+  }
+
+  function handleKeyUp(event: KeyboardEvent) {
+    //console.log("keyup " + event.code);
+    if (event.code == "KeyA" || event.code == "KeyQ") {
+      socket?.emit("playerinput", 0);
+    }
+  }
+
+  return (
+    <>
+      {gameState?.currentState == "Selection" ? (
+        <GameSelection socket={socket} player={player} />
+      ) : (
+        <canvas ref={canvasRef} width={width} height={height} />
+      )}
+    </>
+  );
+}
 
 export default Game;
