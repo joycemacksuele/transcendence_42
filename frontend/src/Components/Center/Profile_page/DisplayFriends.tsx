b64c39e09b4538db6ca85fd9d0ad65085f43b992
jaka <jaka72@gmail.com>
2023-12-07 00:00:22 +0100

    Fixed image upload issue (this.logger) not recognized. Also, replace hardcoded base urls with VITE_BACKEND_URL

--- frontend/src/Components/Center/Profile_page/DisplayFriends.tsx
+++ frontend/src/Components/Center/Profile_page/DisplayFriends.tsx
@@ -37,7 +37,7 @@ const FriendsList: React.FC<FriendsListProps> = ({ clickOnUser }) => {
   // const fetchUsers = async () => {
   // 	try {
   // 		const response = await axiosInstance.get<User[]>(
-  // 			"http://localhost:3001/users/all"
+  // 			"/users/all"
   // 		); // Assuming the server is running on the same host and port
   // 		setUsers(response.data);
   // 		console.log('Jaka, retreived users', response.data);
@@ -49,7 +49,7 @@ const FriendsList: React.FC<FriendsListProps> = ({ clickOnUser }) => {
   const fetchMyId = async () => {
     try {
       const response = await axiosInstance.get(
-        `http://localhost:3001/users/get-user-by-profilename/${localStorage.getItem(
+        `/users/get-user-by-profilename/${localStorage.getItem(
           "profileName"
         )}`
       );
@@ -69,7 +69,7 @@ const FriendsList: React.FC<FriendsListProps> = ({ clickOnUser }) => {
     console.log("Fetch Friends, myId: ", myId);
     try {
       const response = await axiosInstance.get<User[]>(
-        `http://localhost:3001/friendship/${myId}/friends`
+        `/friendship/${myId}/friends`
       );
       setFriends(response.data);
       console.log("Retrieved friends (response.data): ", response.data);
@@ -116,7 +116,7 @@ const FriendsList: React.FC<FriendsListProps> = ({ clickOnUser }) => {
 
   // const deleteDummies = async () => {
   // 	try {
-  // 		await axiosInstance.delete("http://localhost:3001/users/");
+  // 		await axiosInstance.delete("/users/");
   // 		console.log("Dummies deleted successfully");
   // 	} catch (error) {
   // 		console.error("Error deleting dummies: ", error);
@@ -151,7 +151,7 @@ const FriendsList: React.FC<FriendsListProps> = ({ clickOnUser }) => {
                       onClick={(e) => handleUserClick(e, friend.loginName)}
                     >
                       <img
-                        src={"http://localhost:3001/" + friend.profileImage}
+                        src={import.meta.env.VITE_BACKEND_URL + "/" + friend.profileImage}
                         id="profileImage_tiny"
                       />
                       {friend.profileName}
