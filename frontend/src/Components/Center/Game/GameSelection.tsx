74a37ed81574269375dbd624e010e238d507aa11
hman <hman@student.codam.nl>
2023-12-09 12:00:26 +0100

    adjusted the gameselection screen and added some instructions

--- frontend/src/Components/Center/Game/GameSelection.tsx
+++ frontend/src/Components/Center/Game/GameSelection.tsx
@@ -11,24 +11,23 @@ function GameSelection(props: { socket: Socket | null }) {
     props.socket?.emit("joinCustomGame");
   }
   return (
-    <div className="container">
-        <div className="row align-items-center">
-            <div className="col-md-6">test</div>
-        </div>
+    <>
+    <div style={{"height":"80vh"}} className="  d-flex justify-content-center align-items-center">
+<div>
+    <h1>Match Making</h1>
+    <Button variant="dark" onClick={joinDefaultGame}>
+      Play classic Game
+    </Button>
+    <Button variant="dark" onClick={joinCustomGame}>
+      Play custom Game
+    </Button>
+<p></p>
+        <p>Press W and S for paddle movement</p>
+        <p>First one to 3 wins</p>
     </div>
+  </div>
+</>
   );
 }
 
 export default GameSelection;
-
-    // <div className="col-md-12 text-center">
-    //   <h1>Match Making</h1>
-
-    //   <Button variant="dark" onClick={joinDefaultGame}>
-    //     Play classic Game
-    //   </Button>
-    //   <Button variant="dark" onClick={joinCustomGame}>
-    //     Play custom Game
-    //   </Button>
-      
-    // </div>
