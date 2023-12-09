0100dcd1488be486eb4ba32123089d9191a20340
Joyce Macksuele <joycemacksuele@outlook.com>
2023-12-09 13:24:48 +0100

    vite env added to compose

--- frontend/src/Components/Other/AxiosInstance.tsx
+++ frontend/src/Components/Other/AxiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.env.VITE_BACKEND_URL
+	baseURL: import.meta.env.VITE_BACKEND
 });
 
 axiosInstance.interceptors.response.use(
