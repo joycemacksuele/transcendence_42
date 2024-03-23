e392617155e7b181206e2dae6e772e970ee39d1b
Joyce Macksuele <joycemacksuele@outlook.com>
2024-03-23 17:32:08 +0100

    exception passwrod errors fixed

--- backend/src/chat/dto/request-password-related-chat.dto.ts
+++ backend/src/chat/dto/request-password-related-chat.dto.ts
@@ -6,7 +6,8 @@ import {
     MaxLength,
     IsStrongPassword,
     ValidateIf,
-    IsNumber
+    IsNumber,
+    IsEnum
 } from 'class-validator';
 import {ChatType} from '../utils/chat-utils'
 
@@ -14,6 +15,10 @@ export class RequestPasswordRelatedChatDto {
     @IsNumber()
     @IsOptional()
     id: number;
+
+    @IsEnum(ChatType)
+    @IsNotEmpty()
+    type: ChatType;
 
     @IsString()
     @MinLength(3)
@@ -24,6 +29,7 @@ export class RequestPasswordRelatedChatDto {
     // PRIVATE   | is a DM - can't be joined  | only members can see it
     // PUBLIC    | everyone can join it       | everyone can see it
     // PROTECTED | password to join           | everyone can see it
+    @ValidateIf(o => o.type === ChatType.PROTECTED)
     @IsNotEmpty()
     @IsString()
     @IsStrongPassword({ minNumbers: 1, minLength: 5, minUppercase: 1, minSymbols: 0, minLowercase: 1 }, { message: 'password is not following the rules' })
